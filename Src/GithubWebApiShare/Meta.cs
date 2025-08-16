910d1b0cdb7b82c0358ed363f78474b1f085b5dc
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-08-16 20:04:16 +0200

    Github version

--- Src/GithubWebApiShare/Meta.cs
+++ Src/GithubWebApiShare/Meta.cs
@@ -1,11 +1,21 @@
 ﻿namespace GithubWebApi;
 
+/// <summary>
+/// Represents metadata information for the Github Web API, such as the installed version.
+/// </summary>
 public class Meta
 {
-   internal Meta(MetaModel model)
-   {
-      this.InstalledVersion = model.InstalledVersion;
-   }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Meta"/> class using the specified model.
+    /// </summary>
+    /// <param name="model">The model containing metadata information.</param>
+    internal Meta(MetaModel model)
+    {
+        this.InstalledVersion = model.InstalledVersion;
+    }
 
+    /// <summary>
+    /// Gets the installed version of the Github Web API.
+    /// </summary>
     public string? InstalledVersion { get; }
 }
